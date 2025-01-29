594ea9762fd48f4abce81142860b52ad3a0a98e7
Mario Corredor Alarcón <113421851+MarioCorredor@users.noreply.github.com>
2025-01-29 14:52:36 +0100

    pruebas de scheduler

--- main.go
+++ main.go
@@ -78,8 +78,9 @@ func getRandomPokemon() (bson.M, error) {
 func scheduleDailyPokemon() {
 	// Esperar hasta las 14:10 del próximo día
 	now := time.Now()
-	nextScheduledTime := time.Date(now.Year(), now.Month(), now.Day(), 14, 34, 0, 0, now.Location())
+	nextScheduledTime := time.Date(now.Year(), now.Month(), now.Day(), 14, 55, 0, 0, now.Location())
 	durationUntilScheduledTime := nextScheduledTime.Sub(now)
+	fmt.Println("Tiempo de espera: ", durationUntilScheduledTime)
 	time.Sleep(durationUntilScheduledTime)
 	print("Time finished!")
 
