7f9652994900fbdd258dfe0964934e79159eca65
Mario Corredor Alarcón <113421851+MarioCorredor@users.noreply.github.com>
2025-01-29 14:29:46 +0100

    test

--- main.go
+++ main.go
@@ -78,9 +78,11 @@ func getRandomPokemon() (bson.M, error) {
 func scheduleDailyPokemon() {
 	// Esperar hasta las 14:10 del próximo día
 	now := time.Now()
-	nextScheduledTime := time.Date(now.Year(), now.Month(), now.Day(), 14, 26, 0, 0, now.Location())
+	nextScheduledTime := time.Date(now.Year(), now.Month(), now.Day(), 14, 32, 0, 0, now.Location())
+	print(nextScheduledTime)
 	durationUntilScheduledTime := nextScheduledTime.Sub(now)
 	time.Sleep(durationUntilScheduledTime)
+	print("Time finished!")
 
 	// Obtener un Pokémon aleatorio
 	pokemon, err := getRandomPokemon()
